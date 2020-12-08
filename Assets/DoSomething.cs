fb5322715edc686e359f1fdbb42b0fc783e7d6df
Eric Galway <ericwilliamgalway@gmail.com>
2020-12-08 18:26:21 -0500

    Bullet pool created

---
+++ Assets/DoSomething.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoSomething : MonoBehaviour
+{
+    //Variables
+
+    public float gravity = -1.8f;
+    public Vector3 velocity;
+    public Vector3 acceleration;
+
+    public float speed = 0.1f;
+
+    void instatiateValues()
+    {
+        velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        acceleration = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        float frameRate = Time.deltaTime;
+        instatiateValues();
+        acceleration.y = gravity * frameRate;
+       // Debug.LogWarning("Acceleration: " + acceleration);
+       // Debug.LogWarning("Frame Rate: " + frameRate);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity += acceleration;
+       // Debug.LogWarning("Velocity: " + velocity);
+        this.transform.position += velocity;
+    }
+}
---
+++ Assets/InstantiateManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateManager : MonoBehaviour
+{
+    int numBullets = 30;
+    int counter;
+
+    public GameObject[] bulletPool;
+
+    public GameObject prefab;
+    Vector3 bulletPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletPos = new Vector3(0.0f, 0.0f, -10.0f);
+
+
+        for (int i = 0; i < numBullets; i++)
+        {
+            //bulletPos.z += i;
+
+            bulletPool[i] = Instantiate(prefab) as GameObject;
+
+            bulletPool[i].transform.position = bulletPos;
+
+            bulletPool[i].name = "Bullet# " + i;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
